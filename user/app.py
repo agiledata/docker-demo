31dd2b55e5c2c4d1ffa1d3e53a141736fbc1bbfb
Rob Collins <rob.collins@agiledata.io>
2015-05-28 14:40:11 +0100

    Changed name from example_ to dockerdemo_

--- user/app.py
+++ user/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    switch = requests.get("http://example_switch_1:8000").content
-    rule   = requests.get("http://example_rule_1:8001").content
+    switch = requests.get("http://dockerdemo_switch_1:8000").content
+    rule   = requests.get("http://dockerdemo_rule_1:8001").content
     return 'Hello from User Service! \n %s \n %s' % (switch, rule)
 
 if __name__ == '__main__':
